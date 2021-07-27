83d6f4c97359f7835f2677e22de268e25a49ce1c
Yughendaran P <79308218+YughendaranPalanivel@users.noreply.github.com>
2021-07-27 14:05:27 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,20 @@
+### Automatic Number Plate Recognition Using Python and MySQL
+
+A Python application that can recognize vehicle numbers from licence plates and store it in a MySQL database.
+
+## Getting Started
+
+Make sure python, MySQL and tesseract(an OCR engine) is installed locally in your computer.Then, Pip install openCV for accessing webcam and object detection, pytesseract for Optical Character Recognition and mysql.connector for remote or local DB connection.
+
+A few resources to get you started:
+
+Python : https://www.python.org/downloads/
+MySQL : https://www.mysql.com/
+Tesseract : https://github.com/UB-Mannheim/tesseract/wiki
+OpenCV : https://opencv.org/
+pytesseract : https://levelup.gitconnected.com/a-beginners-guide-to-tesseract-ocr-using-pytesseract-23036f5b2211
+mysql.connector : https://dev.mysql.com/doc/connector-python/en/
+
+## Snapshots
+![snapshot](https://i.ibb.co/BV84GfQ/Capture55.png)
+
---
+++ app.py
@@ -0,0 +1,60 @@
+import cv2
+import pytesseract
+import mysql.connector
+from mysql.connector import Error
+from time import strftime
+import os
+
+#Cascade classifier for detecting number plate
+preTrainedModelPath = os.getcwd()+'/haarcascade_russian_plate_number.xml'
+cascodeClassifier = cv2.CascadeClassifier(preTrainedModelPath)
+
+#binding tesseract file path to pytesseract for OCR 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+#config freeDB MySQL database 
+connection = mysql.connector.connect(host='127.168.43.208',
+                                     database='anpl',
+                                     user='root',
+                                     password='Subaa123#')
+if connection.is_connected():
+    db_Info = connection.get_server_info()
+    print("Connected to MySQL Server version ", db_Info)
+    cursor = connection.cursor()
+    cursor.execute("select database();")
+    record = cursor.fetchone()
+    print("You're connected to database: ", record[0])
+
+#accesing webcam using opencv
+rec = cv2.VideoCapture(0)
+
+while(rec.isOpened() and connection.is_connected()):
+    ret, frame = rec.read()
+    gray = cv2.cvtColor(frame, 0)
+    pattern = cascodeClassifier.detectMultiScale(gray,1.1,4)#finding patter inside each frame
+    if(len(pattern)>0):
+        (x,y,w,h) = pattern[0]
+        cropedImage = frame[y:y+h,x:x+w]
+        text = pytesseract.image_to_string(cropedImage).strip()#performing OCR
+        if(text != ''):
+            print(text)
+            try:
+                sqlQuery = """
+                INSERT INTO logs
+                VALUES
+                (DEFAULT,%s, %s)
+                """
+                data = (strftime("%Y-%M-%d %H:%M:%S"),text)
+                cursor.execute(sqlQuery,data)
+                connection.commit()
+            except:
+                connection.rollback()# Rolling back in case of error
+
+        frame = cv2.rectangle(frame,(x, y),(x+w, y+h),(0,255,0),2)#masking
+    cv2.imshow('CAMERA', frame)
+    if(cv2.waitKey(1)&0XFF == ord('q')):
+        break
+
+connection.close()# Closing the connection
+rec.release()#Closing web cam
+cv2.destroyAllWindows()
